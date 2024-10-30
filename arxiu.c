90c19bdfc771f0fe04d47ee4a27dc52b238ae7a8
alanfarre <alanfarre@PCALAN.>
2024-10-30 13:34:41 +0100

    Exercici 6, 7 iniciat

--- arxiu.c
+++ arxiu.c
@@ -1,3 +1,4 @@
+//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
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,23 +8,23 @@ float Mat[N][N], MatDD[N][N], V1[N], V2[N], V3[N], V4[N];
 
 
 void InitData(){ 
-	int i,j; 
-	srand(334411); 
-	for( i = 0; i < N; i++ ) 
-		for( j = 0; j < N; j++ ){ 
+int i,j; 
+srand(334411); 
+for( i = 0; i < N; i++ ) 
+	for( j = 0; j < N; j++ ){ 
 		Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));  
 		if ( (abs(i - j) <= 3) && (i != j)) 
 			MatDD[i][j] = (((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX)); 
 		else if ( i == j ) 
 			MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX))); 
 		else MatDD[i][j] = 0.0; 
-		} 
+	} 
 	for( i = 0; i < N; i++ ){ 
 		V1[i]=(i<N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0; 
 		V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0; 
-		V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))); //Els vectors tenen 256 valors (la mitad de la matriu)
-	} 
-}
+		V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))); 
+} 
+} 
 
 
 void PrintVect( float vect[N], int from, int numel ){//EXERCICI 1
@@ -42,8 +43,7 @@ void PrintRow( float mat[N][N], int row, int from, int numel ) {
 void  MultEscalar( float vect[N], float vectres[N], float alfa ){
 	int i;
         for( i = 0; i < N; i++ )
-		vectres[i] = vect[i]*alfa;
-		
+		vectres[i] = vect[i]*alfa; //Multiplica cada posició del vector pel mateix nombre
 
 }
 
@@ -53,7 +53,7 @@ float Scalar(float vect1[N], float vect2[N]) {
     	for (int i = 0; i < N; i++) {
         	res += vect1[i] * vect2[i]; //S'afegeix al resultat la multiplicació entre les posicions dels vectors
 	}
-	printf("%f", res);
+	return res;
 }
 
 
@@ -62,10 +62,44 @@ float Magnitude( float vect[N] ){
 	for (int i = 0; i < N; i++){
 		res += vect[i]*vect[i]; //Es fa el sumatori de quadrats de totes les posicions
 	}
-	res = sqrt(res); //Es fa l'arrel quadrada del total
-	printf("%f", res);
+	return sqrt(res); //Es fa l'arrel quadrada del total
+	
 }
 
+
+int Ortogonal( float vect1[N], float vect2[N] ){
+	int res;
+	int ortogonal;
+	res = Scalar(vect1, vect2); 
+	if (res == 0){
+		ortogonal = 1;
+	}else{
+		ortogonal = 0;
+	}
+	printf("%d\n", ortogonal);
+}
+
+void  Projection( float vect1[N], float vect2[N], float vectres[N] ){
+	float escalar;
+	float magnitud;
+	escalar = Scalar(vect1, vect2);
+	magnitud = Magnitude (vect2);
+	magnitud = magnitud*magnitud;
+	if (magnitud != 0){
+		float x = escalar / magnitud;
+		for (int i = 0; i < N; i++){
+			vectres[i] = x * vect2[i];
+		}
+	}else{
+		for (int i = 0; i < N; i++){
+			vectres[i] = 0;
+
+		}
+	}
+	for (int i = 0; i < 10; i++){
+	printf("%f\n", vectres[i]);
+	}
+}
 
 
 int main(){
@@ -110,7 +144,7 @@ int main(){
         printf("\n");
         Scalar(V2, V3);
         printf("\n");
-*/
+
 //F
 	Magnitude(V1);
         printf("\n");
@@ -118,7 +152,18 @@ int main(){
         printf("\n");
         Magnitude(V3);
         printf("\n");
+*/
+//G
+/*
+	Ortogonal(V1,V2);
+	Ortogonal(V1,V3);
+        Ortogonal(V2,V3);
+*/
 
+	Projection(V1, V2, vr);
+	Projection(V3, V2, vr);
 }
 
 
+
+	
